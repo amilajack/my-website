b1cfe65cdf7442ccf3eff67a03836cc83097cb55
Amila Welihinda <amilajack@gmail.com>
2020-02-26 10:16:39 -0800

    update config boilerplate

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://amilajack.github.io/my-website/',
   baseUrl: '/',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'amilajack', // Usually your GitHub org/user name.
+  projectName: 'my-website', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'My Site',
@@ -17,7 +17,7 @@ module.exports = {
         {to: 'docs/doc1', label: 'Docs', position: 'left'},
         {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/amilajack/my-website',
           label: 'GitHub',
           position: 'right',
         },
@@ -61,7 +61,7 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/amilajack/my-website',
             },
             {
               label: 'Twitter',
@@ -80,7 +80,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/amilajack/my-website/edit/master/website/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
